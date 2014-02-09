c4a930cb8c3fb5dac3ad81545494dd6e59e10188
Antonis Karamitros <ablackjim228@gmail.com>
2014-02-09 21:49:17 +0000

    middle link aminated

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
 (function(){
 	
+
+	window.myNamespace = {};
+
+	window.where.setNamespace(myNamespace);
+	myNamespace.where.testAll();
+	myNamespace.where.addClassToBody();
+	
+	Object
+	.keys(myNamespace.where.browser)
+	.some(function (key) {
+		if(myNamespace.where.browser[key] === true){
+			document.getElementById('browser').textContent = key;
+			return true;
+		}
+	});
+
+	var osText = document.getElementById('os').textContent;
+
+	Object
+	.keys(myNamespace.where.os)
+	.some(function (key) {
+		if(myNamespace.where.os[key] === true){
+			osText += (osText === '' ? '' : ' ') + key;
+		}
+	});
+	document.getElementById('os').textContent = osText;
+
+
+	//	make links
+	document.getElementById('headerContainer').onclick = function(){ 
+
+		location.href = 'http://www.github.com/antouank/where.js';
+	};
+			
 	var header = document.getElementById('headerContainer'),
 		contentContainer = document.getElementById('contentContainer'),
 		hoverHeaderHandler = function(ev){
