8c213fc7996957669ba20e443c535f5765e996ab
Antonis Karamitros <ablackjim228@gmail.com>
2014-02-15 21:09:26 +0000

    removed Object.keys ( apparently ie7 is too old for me )

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 
 (function(){
 	
+	var oldBrowser = false,
+		_prop;
 
 	//	initialize where in myNamespace
 	window.myNamespace = {};
@@ -10,50 +12,56 @@
 	myNamespace.where.addClassToBody();
 	
 	//	write the browser and OS we are in
-	Object
-	.keys(myNamespace.where.browser)
-	.some(function (key) {
-		if(myNamespace.where.browser[key] === true){
-			document.getElementById('browser').textContent = key;
-			return true;
+	for(_prop in myNamespace.where.browser){
+		if(myNamespace.where.browser.hasOwnProperty(_prop)){
+			if(myNamespace.where.browser[_prop] === true){
+				document.getElementById('browser').textContent = _prop;
+			}
 		}
-	});
+	}
 
 	var osText = document.getElementById('os').textContent;
 
-	Object
-	.keys(myNamespace.where.os)
-	.some(function (key) {
-		if(myNamespace.where.os[key] === true){
-			osText += (osText === '' ? '' : ' ') + key;
+	for(_prop in myNamespace.where.os){
+		if(myNamespace.where.os.hasOwnProperty(_prop)){
+			if(myNamespace.where.os[_prop] === true){
+				osText += (osText === '' ? '' : ' ') + _prop;
+			}
 		}
-	});
+	}
+
 	document.getElementById('os').textContent = osText;
 
+	if( myNamespace.where.browser.ie6 || 
+		myNamespace.where.browser.ie7 ||
+		myNamespace.where.browser.ie8){
 
-	var addTextToP = function(){
-	
-		var p = document.getElementsByTagName('p');
-		typeWriter.giveLife(p[0]);
+		oldBrowser = true;
+		document.getElementById('whereJsLink').classList.remove('hidden');
+	} else {
+		var addTextToP = function(){
+		
+			var p = document.getElementsByTagName('p');
+			typeWriter.giveLife(p[0]);
 
-		p[0].typeWriter.setText('You want to know in which browser and OS your JS is running');
+			p[0].typeWriter.setText('You want to know in which browser and OS your JS is running');
 
-		p[0].typeWriter.startWriting({
-			onEnd: function(){
+			p[0].typeWriter.startWriting({
+				onEnd: function(){
 
-				typeWriter.giveLife(p[1]);
-				p[1].typeWriter.setText('You don\'t want to write the tests in every project');
-				p[1].typeWriter.startWriting({
-					onEnd: function(){
-					 
-						document.getElementById('whereJsLink').classList.remove('hidden');
-					}
-				});
-				typeWriter.takeLife(p[0]);
-			}
-		})
-	};
-
+					typeWriter.giveLife(p[1]);
+					p[1].typeWriter.setText('You don\'t want to write the tests in every project');
+					p[1].typeWriter.startWriting({
+						onEnd: function(){
+						 
+							document.getElementById('whereJsLink').classList.remove('hidden');
+						}
+					});
+					typeWriter.takeLife(p[0]);
+				}
+			})
+		};
+	}
 
 
 	//	make links
@@ -70,7 +78,9 @@
 			this.removeEventListener('mouseover', hoverHeaderHandler);
 			setTimeout(function(){
 			 
-			 	addTextToP();
+			 	if(!oldBrowser){
+			 		addTextToP();
+			 	}
 				contentContainer.className = contentContainer.className.replace(/hidden/, '');
 			},700);
 		};
